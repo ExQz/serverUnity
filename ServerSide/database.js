b4dba329aa19e7526a1003c28638cb1193b0a4bf
Marius <marius.blazys16@gmail.com>
2017-10-09 14:22:42 +0300

    fix merge

---
+++ ServerSide/database.js
@@ -0,0 +1,9 @@
+﻿var mysql = require('mysql');
+var connection = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database:'unitygame'
+});
+
+module.exports = connection;
