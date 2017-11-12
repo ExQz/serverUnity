e113b26862fd0f1da2e16fdcad7535ea15eafa12
Marius <marius.blazys16@gmail.com>
2017-11-13 01:40:10 +0200

    jwt auth implement

--- ServerSide/api/routes/Routers.js
+++ ServerSide/api/routes/Routers.js
@@ -1,6 +1,7 @@
 ﻿var express = require('express');
 var router = express.Router();
 var Model = require('../models/Model');
+var jwt = require('jsonwebtoken');
 
 router.get('/api/:id?', function (req, res, next)
 {
@@ -53,25 +54,30 @@ router.post('/api/', function (req, res, next)
     });
 });
 
-router.delete('/api/:id', function (req, res, next)
+router.delete('/api/:id', ensureToken, function (req, res, next)
 {
-    Model.deleteTask(req.params.id, function (err, rows) {
-        if (err)
-        {
-            res.json(err);
-        }
-        else
-        {
-            if (rows['affectedRows'] === 0) {
-                res.sendStatus(404);
-            }
-            else {
-                res.status(200);
-                res.json(rows);
-            }
-            
+    jwt.verify(req.token, 'my_secret_key', function (err, data) {
+        if (err) {
+            res.sendStatus(403);
+        } else {
+            Model.deleteTask(req.params.id, function (err, rows) {
+                if (err) {
+                    res.json(err);
+                }
+                else {
+                    if (rows['affectedRows'] === 0) {
+                        res.sendStatus(404);
+                    }
+                    else {
+                        res.status(200);
+                        res.json(rows);
+                    }
+
+                }
+            });
         }
     });
+   
 });
 
 router.put('/api/:id', function (req, res, next) {
@@ -95,5 +101,26 @@ router.put('/api/:id', function (req, res, next) {
         }
     });
 });
+
+router.post('/api/login', function (req, res) {
+    const user = { id: 3 };
+    const token = jwt.sign({ user: user.id }, 'my_secret_key');
+    res.json({
+        token: token
+    });
+});
+
+
+function ensureToken(req, res, next) {
+    const bearerHeader = req.headers["authorization"];
+    if (typeof bearerHeader !== 'undefined') {
+        const bearer = bearerHeader.split(" ");
+        const bearerToken = bearer[1];
+        req.token = bearerToken;
+        next();
+    } else {
+        res.sendStatus(403);
+    }
+}
 
 module.exports = router;
